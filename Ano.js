cde5ca3aa42bbaa7963792f6bc82a9b39a12011a
Hama12a <93442863+Hama12a@users.noreply.github.com>
2021-11-05 22:55:09 +0300

    Update Ano.js

--- Ano.js
+++ Ano.js
@@ -21,7 +21,7 @@ var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
 const prefix = "a!";
-client.login("تـــــۆکـــــیــــن لـێرە دانێ");
+client.login("OTA2MjYzMDgwNjUxMDYzMzE2.YYWFQw.ES4S_twtaWVw8te6lekXc47DjrI");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
